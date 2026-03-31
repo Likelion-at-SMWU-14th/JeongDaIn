bb0858b6a2f366fa9047ea7559a002ac8e5f304f
jdain1 <24dd1301@sookmyung.ac.kr>
2026-03-31 21:20:28 +0900

    수정 사항 변경

--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 # Create your views here.
 def signup(request):
@@ -15,8 +15,8 @@ def signup(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get['username']
-        password = request.POST.get['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
@@ -24,3 +24,11 @@ def login(request):
             return redirect('main')
         
     return render(request, 'login.html')
+
+def main(request):
+    return render(request, "main.html")
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+        return redirect('login')
