ed29f1bb340b1b1e1c530c96de427c6a60b158f9
jdain1 <24dd1301@sookmyung.ac.kr>
2026-04-04 21:25:05 +0900

    merge conflict 해결

--- week04/myproject/settings.py
+++ week04/myproject/settings.py
@@ -56,7 +56,6 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
         'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
