62eeba1310b6fc2f72fb26b1af3596476b104bdb
jdain1 <24dd1301@sookmyung.ac.kr>
2026-04-10 21:48:33 +0900

    Feat: 데이터 설정 완료

---
+++ FEweek01_hw/vendingMachine.js
@@ -0,0 +1,10 @@
+// 1. 데이터 설정
+// 음료 리스트 배열로 저장
+// 조건 : 음료-가격 object 형태
+// const 변수명 = { key : value };
+const pay = 10000;  // 지갑 금액 변수 설정
+const bev = [{ name: "아메리카노", price: 3000},
+    { name: "카페라떼", price: 4000},
+    { name: "카푸치노", price: 4500},
+    { name: "녹차라떼", price: 6000},
+    { name: "딸기라떼", price: 5500},];
