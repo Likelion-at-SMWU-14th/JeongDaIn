d437deb7a2c62e9d6c557feedf39eb7deb5983a8
jdain1 <24dd1301@sookmyung.ac.kr>
2026-04-10 22:05:19 +0900

    Feat: 음료 구매 함수 구현

--- FEweek01_hw/vendingMachine.js
+++ FEweek01_hw/vendingMachine.js
@@ -2,7 +2,7 @@
 // 음료 리스트 배열로 저장
 // 조건 : 음료-가격 object 형태
 // const 변수명 = { key : value };
-const pay = 10000;  // 지갑 금액 변수 설정
+let pay = 10000;  // 지갑 금액 변수 설정
 const bev = [{ name: "아메리카노", price: 3000},
     { name: "카페라떼", price: 4000},
     { name: "카푸치노", price: 4500},
@@ -12,5 +12,20 @@ const bev = [{ name: "아메리카노", price: 3000},
 // 2. 랜덤 선택 기능
 // Math 메서드를 사용해서 무작위로 음료 1개 선택
 function getRandom(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
+    return Math.floor(Math.random() * arr.length);
 }
+
+// 3. 출력 조건
+// 구매 가능하다면 음료 출력, 지갑에서 price만큼 빼기
+// 구매 불가능하다면 잔고 부족 메세지 출력
+function buyBev(n) {
+    if (bev[n].price <= pay) {
+        pay -= bev[n].price;
+        console.log(`${bev[n].name} 음료가 나왔어요! (가격:${bev[n].price}원)`);
+    }
+    else {console.log(`돈이 부족해요! 음료를 살 수 없어요!`);
+
+    }
+}
+
+buyBev(getRandom(bev));
